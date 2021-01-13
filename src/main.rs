efbb916ccc4c94bd02a1da620ab41e3c917f8b3d
Hocine <ocineh@hotmail.com>
2021-01-13 22:22:00 +0100

    gestion de l'erreur quand il n'y a pas assez d'argument
    lors de l'utilisation du constructeur de Config verifie que le tableau de args n'a pas une valeur inferieur a 3 si c'est le cas alors le programme panic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,7 +23,8 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config { query: args[1].clone(), filename: args[2].clone() }
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 { return Err("not enough arguments"); }
+        Ok(Config { query: args[1].clone(), filename: args[2].clone() })
     }
 }
