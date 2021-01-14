9550703023af1a5cde0e41b31bcb6fef95d0f8da
Hocine <ocineh@hotmail.com>
2021-01-14 09:33:52 +0100

    ajout de la fonction search et de test unitaire
    la fonction search cherche dans le contenue du fichier ligne par ligne et si elle trouve le texte recherche elle l'ajoute a un vecteur qu'elle renvoie a la fin.
    ajout de 3 test unitaire pour tester la fonction search.

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,42 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // if fs::read_to_string returns an Err it is directly returned as the result of the run function thanks to the ?
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+// you have to indicate the shelf life to remove any cramping, the return and a slice of the content so they must have the same shelf life
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) { results.push(line); }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_one_result() {
+        let query = "duct";
+        let contents = "\nRust:\nsafe, fast, productive\nPick three.";
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+    #[test]
+    fn search_no_result() {
+        let query = "nothing";
+        let contents = "\nRust:\nsafe, fast, productive\nPick three.";
+        let result: Vec<&str> = Vec::new();
+        assert_eq!(result, search(query, contents));
+    }
+    #[test]
+    fn search_two_result() {
+        let query = "st";
+        let contents = "\nRust:\nsafe, fast, productive\nPick three.";
+        assert_eq!(vec!["Rust:", "safe, fast, productive"], search(query, contents));
+    }
 }
